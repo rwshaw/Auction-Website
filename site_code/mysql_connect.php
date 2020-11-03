83b9b157830ff501fa363c63325c17d73ee5271c
Qasim Razvi <qr@MacBook-Pro.local>
2020-11-03 13:04:44 +0000

    sql connect fuctions - open, close - with website user login

---
+++ site_code/mysql_connect.php
@@ -0,0 +1,28 @@
+<!-- This won't be served as a file on front end. Include this as an import in each file that needs to connect to DB. -->
+
+<?php
+// Connecting to DB
+
+function OpenDbConnection() {
+    $servername = "localhost";
+    $user = "website";
+    $pass = "3ZqpGsAsmC6U2opZ";
+    /*
+    // Root user details
+    $rootuser = "root";
+    $rootpass = "";
+    */
+
+    $connect = new mysqli($servername, $user, $pass);
+
+    if ($connect -> error) {
+        die("Connection to DB failed: " . $connect->connect_error);
+    }
+    return $connect;
+}
+
+function CloseDbConnection($connection) {
+    $connection -> close();
+}
+
+?>
