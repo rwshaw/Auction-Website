8c2fe491e3406f1934845c5278e6c91fd95814dc
rwshaw <rwshaw97@gmail.com>
2020-11-07 14:59:53 +0000

    watchlist_func

--- site_code/watchlist_funcs.php
+++ site_code/watchlist_funcs.php
@@ -1,3 +1,12 @@
+<?php 
+require("mysql_connect.php");
+require("debug.php");
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+?>
+
+
  <?php
 
 if (!isset($_POST['functionname']) || !isset($_POST['arguments'])) {
@@ -5,12 +14,17 @@ if (!isset($_POST['functionname']) || !isset($_POST['arguments'])) {
 }
 
 // Extract arguments from the POST variables:
-$item_id = $_POST['arguments'];
+$item_id = implode($_POST['arguments']); 
 
 if ($_POST['functionname'] == "add_to_watchlist") {
   // TODO: Update database and return success/failure.
-
-  $res = "success";
+	// To do for Q: load $session userID and pass for the query and poss some default vals for the other fields
+	$connection = OpenDbConnection(INSERTSESSIONVARSandREMOVEFUNCHARDCODING);
+	error_log(error_log( print_r($connection, TRUE) ));
+	$query = "INSERT INTO watchlist (userID, listingID, bidID)"."VALUES (INSERTSESSIONVAR,$item_id,INSERTSESSIONVAR)";
+	$result = mysqli_query($connection,$query) or die('Error making saveToDatabase query');
+	CloseDbConnection($connection);
+	$res = "success";
 }
 else if ($_POST['functionname'] == "remove_from_watchlist") {
   // TODO: Update database and return success/failure.
