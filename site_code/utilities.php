92e7eada57d5cdfd81a473f6bc9723dc2b83539a
Qasim Razvi <qr@MacBook-Pro.local>
2020-11-10 15:23:17 +0000

    ALL watchlist functions working. email needs tests

--- site_code/utilities.php
+++ site_code/utilities.php
@@ -1,76 +1,76 @@
+<?php require_once("mysql_connect.php");
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);?>
+
 <?php
 
 // display_time_remaining:
 // Helper function to help figure out what time to display
-function display_time_remaining($interval) {
+function display_time_remaining($interval)
+{
 
-    if ($interval->days == 0 && $interval->h == 0) {
-      // Less than one hour remaining: print mins + seconds:
-      $time_remaining = $interval->format('%im %Ss');
-    }
-    else if ($interval->days == 0) {
-      // Less than one day remaining: print hrs + mins:
-      $time_remaining = $interval->format('%hh %im');
-    }
-    else {
-      // At least one day remaining: print days + hrs:
-      $time_remaining = $interval->format('%ad %hh');
-    }
+  if ($interval->days == 0 && $interval->h == 0) {
+    // Less than one hour remaining: print mins + seconds:
+    $time_remaining = $interval->format('%im %Ss');
+  } else if ($interval->days == 0) {
+    // Less than one day remaining: print hrs + mins:
+    $time_remaining = $interval->format('%hh %im');
+  } else {
+    // At least one day remaining: print days + hrs:
+    $time_remaining = $interval->format('%ad %hh');
+  }
 
   return $time_remaining;
-
 }
 
 // print_listing_li:
 // This function prints an HTML <li> element containing an auction listing
-function print_listing_li($item_id, $title, $desc, $price, $num_bids, $end_time,$category, $subcategory)
+function print_listing_li($item_id, $title, $desc, $price, $num_bids, $end_time, $category, $subcategory)
 {
   // Truncate long descriptions
   if (strlen($desc) > 250) {
     $desc_shortened = substr($desc, 0, 250) . '...';
-  }
-  else {
+  } else {
     $desc_shortened = $desc;
   }
-  
+
   // Fix language of bid vs. bids
   if ($num_bids == 1) {
     $bid = ' bid';
-  }
-  else {
+  } else {
     $bid = ' bids';
   }
-  
+
   // Calculate time to auction end
 
   $end_time = DateTime::createFromFormat('Y-m-d H:i:s', $end_time); //reformat date from string to datetime.
   $now = new DateTime();
   if ($now > $end_time) {
     $time_remaining = 'This auction has ended';
-  }
-  else {
+  } else {
     // Get interval:
     $time_to_end = date_diff($now, $end_time);
     $time_remaining = display_time_remaining($time_to_end) . ' remaining';
   }
-  
+
   // Print HTML
-  echo('
+  echo ('
     <li class="list-group-item d-flex justify-content-between">
     <div class="p-2 mr-5">
     <h5><a href="listing.php?item_id=' . $item_id . ' aria-labelledby="catTree">' . $title . '</a>
     <small id=catTree class="text-muted font-italic">
-    ' . $category. ' -> ' . $subcategory . '
+    ' . $category . ' -> ' . $subcategory . '
   </small>
     </h5>
    ' . $desc_shortened . '</div>
     <div class="text-center text-nowrap"><span style="font-size: 1.5em">£' . number_format($price, 2) . '</span><br/>' . $num_bids . $bid . '<br/>' . $time_remaining . '</div>
-  </li>'
-  );
+  </li>');
 }
 
 //HTML BOOTSTRAP Alert Generator.
-function  print_alert($alert_type, $alert_heading, $alert_text1, $alert_text2) {
+function  print_alert($alert_type, $alert_heading, $alert_text1, $alert_text2)
+{
   /**
    * Creates an alert message.
    * 
@@ -80,8 +80,8 @@ function  print_alert($alert_type, $alert_heading, $alert_text1, $alert_text2) {
    * @param string $alert_text2 - Below margin text (already put between p tags)
    * @return string html alert
    */
-  echo 
-  '<div class="alert alert-' . $alert_type . '" alert-dismissable fade show role="alert">
+  echo
+    '<div class="alert alert-' . $alert_type . '" alert-dismissable fade show role="alert">
     <h4 class="alert-heading">' . $alert_heading . '</h4>
     <p>' . $alert_text1 . '    
     <button type="button" class="close" data-dismiss="alert" aria-label="Close">
@@ -92,5 +92,23 @@ function  print_alert($alert_type, $alert_heading, $alert_text1, $alert_text2) {
   </div>';
 }
 
+function send_user_email($user_id, $subject, $message)
+{ // email currently not working for Q on mac...
+  $headers = array();
+  $headers['MIME-Version'] = '1.0';
+  $headers['Content-type'] = 'text/html; charset=iso-8859-1';
+  $headers['From'] = 'team@auctionxpress.com';
 
-?>
+  //get user email from user_id
+  $user_email_query = "SELECT email FROM users WHERE userID = $user_id";
+  $result = SQLQuery($user_email_query);
+  $user_email = $result[0]["email"];
+  $message = wordwrap($message,70,"\r\n"); // set character wrap in case lines > 70
+
+  $success = mail($user_email,$subject,$message,$headers);
+  if (!$success) {
+    $errmsg = error_get_last()['message'];
+  }
+  return $success;
+
+}
---
+++ site_code/watchlist_notifications.php
@@ -0,0 +1,65 @@
+<?php require_once("mysql_connect.php");
+require_once("debug.php");
+require_once("utilities.php");
+?> 
+ 
+
+ <?php
+
+
+if (!isset($_POST['functionname']) || !isset($_POST['arguments'])) {
+  return;
+}
+
+// Extract arguments from the POST variables:
+$item_id = implode('',$_POST['arguments']);
+
+// GET session userID
+$userid =  4; //user 4 = Qasim - testing  (should get this from session later. //$_SESSION['userID'];
+
+//  SEND EMAIL for successful removal from watchlist
+if ($_POST['functionname'] == "remove_watch_email") {
+    // Send email of successful removal from watchlist
+    // get item name
+    $item_name_query = "SELECT itemName FROM auction_listing WHERE listingID=$item_id";
+    $item_result = SQLQuery($item_name_query);
+    $itemName = $item_result[0]["itemName"];
+
+    // TODO create subject + html message.
+    $subject = "Item removed from watchlist";
+
+    $message = "<html>
+    <h2>That's all done!</h2>
+    <p><span style=\"color: #008000;\">$itemName</span> has been <span style=\"color: #008000;\">successfullly removed from your watchlist</span>.</p>
+    <p>To see items you currently have in your watchlist, go to the 'My Watchlist' tab from the navigation bar.</p>
+    <p><em>Happy Buying!</em></p>
+    <p><em>The AuctionXpress Team</em></p></html>";
+
+    $success = send_user_email($userid,$subject,$message);
+
+    echo $success;
+
+} elseif ($_POST['functionname'] == "add_watch_email") {
+    // Send email of successfull add to watchlist
+   // get item name
+    $item_name_query = "SELECT itemName FROM auction_listing WHERE listingID=$item_id";
+    $item_result = SQLQuery($item_name_query);
+    $itemName = $item_result[0]["itemName"];
+
+    // TODO create subject + html message.
+    $subject = "New item added to watchlist";
+
+    $message = "<html>
+    <h2>Great News!</h2>
+    <p><span style=\"color: #008000;\">$itemName</span> has been <span style=\"color: #008000;\">successfullly added to your watchlist</span>.</p>
+    <p>To see items you currently have in your watchlist, go to the 'My Watchlist' tab from the navigation bar.</p>
+    <p><em>Happy Buying!</em></p>
+    <p><em>The AuctionXpress Team</em></p></html>";
+
+    $success = send_user_email($userid,$subject,$message);
+
+    echo $success;
+
+}
+
+?>
